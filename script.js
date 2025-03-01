3ba2b50d249a57a97592c00e6aee041b43fa7fc9
Evgeshka23 <143422172+Evgeshka23@users.noreply.github.com>
2025-03-01 16:35:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+//Task 1
+// let num;
+// (num > 0) ? console.log(true) : console.log(false);
+
+//Task 2
+// let str;
+// (str === 'test') ? console.log(true) : console.log(false);
+
+//Task 3
+// let num;
+// (num > 10) ? console.log(num -5) : console.log(num + 5);
+
+//Task 4
+// let month = prompt('Enter month number');
+// switch (month) {
+//     case '1':
+//         console.log('January');
+//         break;
+//     case '2':
+//         console.log('February');
+//         break;
+//     case '3':
+//         console.log('March');
+//         break;
+//     case '4':
+//         console.log('April');
+//         break;
+//     case '5':
+//         console.log('May');
+//         break;
+//     case '6':
+//         console.log('June');
+//         break;
+//     case '7':
+//         console.log('July');
+//         break;
+//     case '8':
+//         console.log('August');
+//         break;
+//     case '9':
+//         console.log('September');
+//         break;
+//     case '10':
+//         console.log('October');
+//         break;
+//     case '11':
+//         console.log('November');
+//         break;
+//     case '12':
+//         console.log('December');
+//         break;
+//     default:
+//         console.log('Invalid month number');
+//         break;
+// }
+
+//Task 5
+// let number = prompt("Введіть тризначне число:");
+
+// let a = Number(number[0]); 
+// let b = Number(number[1]); 
+// let c = Number(number[2]); 
+
+// let sum = a + b + c;
+
+// console.log(`Сума цифр: ${sum}`);
+
